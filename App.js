8c7f4ff3a8c0457ca8c152ea7bb6bd5342e4e001
Ran Levi <ran.levi@gmail.com>
2021-09-03 17:14:32 +0300

    exploration.

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import {Box, NativeBaseProvider, Input } from 'native-base';
+
+/*
+A platform for text chat game. 
+Anyone can use it for what ever game they want.
+Everything is turn-based, because it's natural.
+
+*/
+
+function TextBallon(props){
+
+  return (
+    <Box 
+      bg="primary.100"
+    >
+      {props.text}
+    </Box>
+  )
+}
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NativeBaseProvider>
       <StatusBar style="auto" />
-    </View>
+      <Box style={styles.container}>
+        <TextBallon text="You see a spider." />
+        <Input 
+          selectTextOnFocus={true}
+          isFullWidth={true}
+          onSubmitEditing={(event)=>{
+            if (event.nativeEvent.text==="ה"){
+              console.log("GGG");
+            }
+            
+          }}
+        />
+        
+      </Box>
+    </NativeBaseProvider>
   );
 }
 
